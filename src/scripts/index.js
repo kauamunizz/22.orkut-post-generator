a77b9cfcdef90ffa91d069863be9476f900c308f
Kauã Muniz <kauamunizz@hotmail.com>
2022-09-15 19:06:23 -0300

    att

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,67 @@
 import '../styles/styles.scss';
+import html2canvas from 'html2canvas';
 
+"strict mode"
+
+const index = (() => {
+
+    async function download() {
+        const { titulo } = document.forms.createForm;
+
+        const preview = document.querySelector('.direito');
+        const canvas = await html2canvas(preview);
+        const dataImg = canvas.toDataURL('image/png');
+
+        const downloadLink = document.createElement('a');
+        downloadLink.download = titulo.value;
+        downloadLink.href = dataImg;
+        downloadLink.click();
+    }
+
+    function handleOpenDialog() {
+        const dialog = document.createElement('input');
+        dialog.type = 'file';
+        dialog.accept = 'image/*';
+        dialog.click();
+    
+        dialog.onchange = () => {
+            const image = dialog.files[0];
+            const img = new Image();
+            img.src = URL.createObjectURL(image);
+            
+            document.querySelector('.newImage').prepend(img);
+        }
+    }
+
+    function events() {
+
+        document.querySelector('.submit').addEventListener('click', event => {
+            event.preventDefault();
+            download();
+        })
+        
+        document.querySelector('.input-group').addEventListener('click', handleOpenDialog)
+
+        document.forms.createForm.addEventListener('input', () => {
+            const { titulo, autor, descricao } = document.forms.createForm;
+
+            document.querySelector(".titulo").innerHTML =  titulo.value;
+            document.querySelector(".tituloMaior").innerHTML =  titulo.value;
+            document.querySelector(".autor").innerHTML = '@' + autor.value;
+            document.querySelector(".descricao").innerHTML =  descricao.value;
+        });
+    }
+
+
+    function init() {
+        
+        events();
+    }
+
+
+    return {
+        init
+    }
+})();
+
+document.addEventListener('DOMContentLoaded', index.init);
